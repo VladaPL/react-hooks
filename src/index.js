10ebe9b3e45c444a215d8afc1202a82538b5d004
Vlada Plutalova <vladaplutalova@yandex.ru>
2023-01-09 16:11:32 +0300

    refactor: refactor and add catch block

--- src/index.js
+++ src/index.js
@@ -52,11 +52,33 @@ const getPlanet = (id) => {
 
 const useRequest = (request) => {
     // аргумент request - это ф-ия, которая возвращает промис.
-    const [dataState, setDataState] = useState(null);
+
+    const initialState = {
+        data: null,
+        loading: true,
+        error: null,
+    };
+
+    const [dataState, setDataState] = useState(initialState);
 
     useEffect(() => {
+        setDataState(initialState);
         let cancelled = false;
-        request().then((data) => !cancelled && setDataState(data));
+        request()
+            .then(
+                (data) =>
+                    !cancelled &&
+                    setDataState({ data, loading: false, error: null })
+            )
+            .catch(
+                (error) =>
+                    !cancelled &&
+                    setDataState({
+                        data: null,
+                        loading: false,
+                        error,
+                    })
+            );
         return () => (cancelled = true);
     }, [request]);
 
@@ -73,11 +95,19 @@ const usePlanetInfo = (id) => {
 };
 
 const PlanetInfo = ({ id }) => {
-    const data = usePlanetInfo(id);
+    const { data, loading, error } = usePlanetInfo(id);
+
+    if (error) {
+        return <div>Что-то пошло не так...</div>;
+    }
+
+    if (loading) {
+        return <div>Идет заграузка данных...</div>;
+    }
 
     return (
         <div>
-            {id} - {data && data.name}
+            {id} - {data.name}
         </div>
     );
 };
